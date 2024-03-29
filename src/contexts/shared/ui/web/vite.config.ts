13fed8169a5d1d711853df6acf694342c3cd14fb
Jose Veliz <ducen29@gmail.com>
2024-03-28 22:33:00 -0400

    Fix command

--- src/contexts/shared/ui/web/vite.config.ts
+++ src/contexts/shared/ui/web/vite.config.ts
@@ -2,10 +2,11 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -15,8 +16,8 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: '@shared/ui',
-      fileName: 'index',
-      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
