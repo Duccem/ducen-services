aa0ed72d7de73c37c15de808827b1254d436be0c
Jose Veliz <ducen29@gmail.com>
2024-03-28 23:03:51 -0400

    Testing vercel

--- src/contexts/shared/ui/web/vite.config.ts
+++ src/contexts/shared/ui/web/vite.config.ts
@@ -2,11 +2,10 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -15,9 +14,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: '@shared/ui',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs', 'umd'],
+      name: '@shared/ui-web',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
