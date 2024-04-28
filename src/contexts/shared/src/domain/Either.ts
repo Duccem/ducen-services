2a04f5322edfc2761efaa45f6b91bd803e37e8f9
Jose Veliz <ducen29@gmail.com>
2024-04-28 03:19:01 -0400

    Better Either

--- src/contexts/shared/src/domain/Either.ts
+++ src/contexts/shared/src/domain/Either.ts
@@ -1,5 +1,3 @@
-import { Nullable } from './Types/Nullable';
-
 type Left<L> = { kind: 'Left'; value: L };
 type Right<R> = { kind: 'Right'; value: R };
 type EitherValue<L, R> = Left<L> | Right<R>;
@@ -7,15 +5,25 @@ type EitherValue<L, R> = Left<L> | Right<R>;
 export class Either<L, R> {
   private constructor(private readonly value: EitherValue<L, R>) {}
 
+  /**
+   * @returns false if Right or returns the result of the given predicate to the Left value.
+   */
   isLeft(): boolean {
     return this.value.kind === 'Left';
   }
 
+  /**
+   * @returns false if Left or returns the result of the given predicate to the Right value.
+   */
   isRight(): boolean {
     return this.value.kind === 'Right';
   }
-
-  map<T>(leftFn: (value: L) => T, rightFn: (value: R) => T): T {
+  /**
+   * Transform an Either into a value of C. Alternative to using when to fold an Either into a value C.
+   * @param leftFn - A function that takes the left value of this Either and returns a value of type C.
+   * @param rightFn - A function that takes the right value of this Either and returns a value of type C.
+   */
+  fold<T>(leftFn: (value: L) => T, rightFn: (value: R) => T): T {
     switch (this.value.kind) {
       case 'Left':
         return leftFn(this.value.value);
@@ -24,22 +32,67 @@ export class Either<L, R> {
     }
   }
 
-  mapValue<T>(fn: (value?: R) => T): Nullable<T> {
-    return this.map(
-      () => null,
+  /**
+   * Map, or transform, the right value B of this Either to a new value C.
+   * @param fn - A function that takes the right value of this Either and returns a new value of type C.
+   */
+  map<T>(fn: (value: R) => T): Either<L, T> {
+    return this.fold(
+      (leftValue) => Either.Left(leftValue),
+      (rightValue) => Either.Right(fn(rightValue)),
+    );
+  }
+
+  /**
+   * Map, or transform, the right value B of this Either into a new Either with a right value of type C. Returns a new Either with either the original left value of type A or the newly transformed right value of type C.
+   * @param fn - A function that takes the right value of this Either and returns a new Either with a right value of type C.
+   * @returns
+   */
+  mapFlat<T>(fn: (value: R) => Either<L, T>): Either<L, T> {
+    return this.fold(
+      (leftValue) => Either.Left(leftValue),
       (rightValue) => fn(rightValue),
     );
   }
 
-  mapError<T>(fn: (value: L) => T): Nullable<T> {
-    return this.map(
-      (leftValue) => fn(leftValue),
-      () => null,
+  /**
+   * Map, or transform, the left value A of this Either to a new value B.
+   * @param fn - A function that takes the left value of this Either and returns a new value of type B.
+   */
+  mapLeft<T>(fn: (value: L) => T): Either<T, R> {
+    return this.fold(
+      (leftValue) => Either.Left(fn(leftValue)),
+      (rightValue) => Either.Right(rightValue),
     );
   }
 
+  /**
+   * Map, or transform, the left value A of this Either into a new Either with a left value of type B. Returns a new Either with either the original right value of type A or the newly transformed left value of type B.
+   * @param fn - A function that takes the left value of this Either and returns a new Either with a left value of type B.
+   * @returns
+   */
+  mapLeftFlat<T>(fn: (value: L) => Either<T, R>): Either<T, R> {
+    return this.fold(
+      (leftValue) => fn(leftValue),
+      (rightValue) => Either.Right(rightValue),
+    );
+  }
+
+  /**
+   * @returns the right value of this Either if it exists, otherwise returns null.
+   */
+  getOrNull(): R {
+    return this.fold(
+      () => null,
+      (rightValue) => rightValue,
+    );
+  }
+
+  /**
+   * @returns the right value of this Either if it exists, otherwise throws an error.
+   */
   getOrThrow(error?: L): R {
-    return this.map(
+    return this.fold(
       (leftValue) => {
         if (error) throw error;
         throw leftValue;
@@ -48,14 +101,57 @@ export class Either<L, R> {
     );
   }
 
+  /**
+   * @returns the right value of this Either if it exists, otherwise returns the default value.
+   */
   getOrElse(defaultValue: R): R {
-    return this.map(
+    return this.fold(
       () => defaultValue,
       (someValue) => someValue,
     );
   }
 
-  static async asyncTryCatch<L, T>(fn: Promise<T>, error?: L): Promise<Either<L, T>> {
+  /**
+   * @returns the left value of this Either if it exists, otherwise returns null.
+   */
+  getLeftOrNull(): L {
+    return this.fold(
+      (leftValue) => leftValue,
+      () => null,
+    );
+  }
+
+  /**
+   * Performs the given action on the right value of this Either if it exists.
+   */
+  onRight(fn: (value: R) => void): Either<L, R> {
+    if (this.isRight()) {
+      fn(this.getOrNull());
+    }
+    return this;
+  }
+
+  /**
+   * Performs the given action on the left value of this Either if it exists.
+   */
+  onLeft(fn: (value: L) => void): Either<L, R> {
+    if (this.isLeft()) {
+      fn(this.getLeftOrNull());
+    }
+    return this;
+  }
+
+  /**
+   * Swaps the left and right values of this Either.
+   */
+  swap(): Either<R, L> {
+    return this.fold(
+      (leftValue) => Either.Right(leftValue),
+      (rightValue) => Either.Left(rightValue),
+    );
+  }
+
+  static async asyncTryCatch<L, R>(fn: Promise<R>, error?: L): Promise<Either<L, R>> {
     try {
       return Either.Right(await fn);
     } catch (_) {
@@ -63,7 +159,7 @@ export class Either<L, R> {
     }
   }
 
-  static tryCatch<L, T>(fn: () => T, error?: L): Either<L, T> {
+  static tryCatch<L, R>(fn: () => R, error?: L): Either<L, R> {
     try {
       return Either.Right(fn());
     } catch (_) {
